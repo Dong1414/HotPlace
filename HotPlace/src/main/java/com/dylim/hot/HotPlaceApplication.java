772c6786303045eb2c9718f31b7101dc78709e5f
Dong1414 <Dong1414@github.com>
2022-10-11 23:19:20 +0900

    init

---
+++ HotPlace/src/main/java/com/dylim/hot/HotPlaceApplication.java
@@ -0,0 +1,13 @@
+package com.dylim.hot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HotPlaceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HotPlaceApplication.class, args);
+	}
+
+}
---
+++ HotPlace/src/test/java/com/dylim/hot/HotPlaceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dylim.hot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HotPlaceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
